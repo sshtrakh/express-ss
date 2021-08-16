a7561831b4840d09b0a363fb3040e98d2ff7bfe5
Sveta <s.shtrah@gmail.com>
2021-08-16 18:55:05 +0300

    added post put

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 5000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//Members API routes
+// Members API routes
 app.use('/api/members', require('./routes/api/members'));
 
 app.listen(PORT, () => {
