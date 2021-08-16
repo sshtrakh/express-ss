c3df39b09707cce55bc360c403bf42eed6a7028b
Sveta <s.shtrah@gmail.com>
2021-08-16 18:42:14 +0300

    added post put

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const {json} = require("express");
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json()); // to handle json
+app.use(express.urlencoded({ extended: false })); // to handle forms
 
 const PORT = 5000;
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/api/members.js
+++ routes/api/members.js
@@ -2,13 +2,28 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 const members = require('../../Members');
+const uuid = require('uuid');
+const e = require("express");
 
-//get all members
+// get all members
 router.get('/', (req, res) => res.json(members));
 
-//post new member
+// post new member
 router.post('/', (req, res) => {
-    res.send(req.body);
+
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active',
+    };
+
+    if (!req.body.email || !req.body.email) {
+        return res.status(400).json({msg: 'Please provide email and name'});
+    } else {
+        members.push(newMember);
+        res.json(newMember);
+    }
 })
 
 //get member by id
@@ -17,8 +32,31 @@ router.get('/:id', (req, res) => {
     if (found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id)));
     } else {
-        res.status(400).json({ msg: 'no member with such id found'});
+        res.status(400).json({msg: 'no member with such id found'});
     }
+});
+
+
+// update member by id
+router.put('/:id', (req, res) => {
+    const found = members.some(v => v.id === parseInt(req.params.id));
+
+    if (found) {
+        const updatedMember = req.body;
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+
+                member.email = updatedMember.email ? updatedMember.email : member.email;
+                member.name = updatedMember.name ? updatedMember.name : member.name;
+
+                res.json(member);
+            }
+        });
+
+    } else {
+        res.status(400).json({msg: `No member found with the id ${req.params.id}`});
+    }
+
 });
 
 module.exports = router;
